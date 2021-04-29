a94054deb8c4ab47f8384da6cbff45bb935bfb0a
julia <juliabresolin2@gmail.com>
2021-04-29 19:17:33 -0300

    (classes) 03: sign in button component

---
+++ classes/cap_03/ignews/src/components/SignInButton/index.tsx
@@ -0,0 +1,23 @@
+import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi'
+import styles from './styles.module.scss'
+
+export function SignInButton() {
+  const isUserLoggedIn = true;
+
+  return isUserLoggedIn ? 
+  (
+    <button type="button" className={styles.button}>
+      <FaGithub color="#04d361"/>
+      Julia Bresolin
+      <FiX color="#737380" className={styles.closeIcon}/>
+    </button>
+  ) 
+  : 
+  (
+    <button type="button" className={styles.button}>
+      <FaGithub color="#eba417"/>
+      Sign in with Github
+    </button>
+  )
+}
