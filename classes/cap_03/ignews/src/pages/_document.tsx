abbe8899c683388e4ca05acbf495604a12f9654d
julia <juliabresolin2@gmail.com>
2021-04-29 19:36:48 -0300

    (classes) 03: homepage create

--- classes/cap_03/ignews/src/pages/_document.tsx
+++ classes/cap_03/ignews/src/pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Head, Html, Main, NextScript } from 'next/document'
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang="pt">
+      <Html lang="en">
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
--- classes/cap_03/ignews/src/pages/index.tsx
+++ classes/cap_03/ignews/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import styles from '../styles/pages/home.module.scss';
 
 export default function Home() {
   return (
@@ -6,6 +7,17 @@ export default function Home() {
       <Head>
         <title>ig.news | home</title>
       </Head>
+      <main className={styles.container}>
+        <section>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br /> 
+            <span>for $9.90/month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="girl coding image"/>
+      </main>
     </>
   )
 }
