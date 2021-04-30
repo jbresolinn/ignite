e5620335cc6e8346852466e248e7f4a0364ef6fa
julia <juliabresolin2@gmail.com>
2021-04-30 10:01:04 -0300

    (classes) 03: github oauth with next-auth

--- classes/cap_03/ignews/src/components/SignInButton/index.tsx
+++ classes/cap_03/ignews/src/components/SignInButton/index.tsx
@@ -1,23 +1,35 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/client'
+
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? 
-  (
-    <button type="button" className={styles.button}>
-      <FaGithub color="#04d361"/>
-      Julia Bresolin
-      <FiX color="#737380" className={styles.closeIcon}/>
-    </button>
-  ) 
-  : 
-  (
-    <button type="button" className={styles.button}>
-      <FaGithub color="#eba417"/>
+  console.log(session);
+
+  return session ?
+    (
+      <button
+        type="button"
+        className={styles.button}
+        onClick={() => signOut()}
+      >
+        <FaGithub color="#04d361" />
+        {session.user.name}
+        <FiX color="#737380" className={styles.closeIcon} />
+      </button>
+    )
+    :
+    (
+      <button
+        type="button"
+        className={styles.button}
+        onClick={() => signIn('github')}
+      >
+        <FaGithub color="#eba417" />
       Sign in with Github
-    </button>
-  )
+      </button>
+    )
 }
