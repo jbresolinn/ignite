7b95413090c4b19503e63ab5df0129f50fcedf43
julia <juliabresolin2@gmail.com>
2021-05-01 14:51:37 -0300

    (classes) 03: prismic cms integration

--- classes/cap_03/ignews/src/components/Header/index.tsx
+++ classes/cap_03/ignews/src/components/Header/index.tsx
@@ -1,14 +1,22 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { ActiveLink } from '../ActiveLink'
 import { SignInButton } from '../SignInButton'
 import styles from './styles.module.scss'
 
 export function Header() {
+
   return (
     <header className={styles.container}>
       <div>
-        <img src="/images/logo.svg" alt="ig.news logo"/>
+        <img src="/images/logo.svg" alt="ig.news logo" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts">
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
